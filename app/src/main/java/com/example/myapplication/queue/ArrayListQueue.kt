d6107c451a923370876c29aadcd814daaff3dc98
Konstantinopolis <zehrakarakaya999@gmail.com>
2023-01-08 22:19:27 +0300

    ekler

---
+++ app/src/main/java/com/example/myapplication/queue/ArrayListQueue.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.queue
+
+class ArrayListQueue <E>: Queue<E>{
+    override fun peek(): E? {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/queue/Queue.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.queue
+
+interface Queue <E>
+    fun peek():E?
+}
